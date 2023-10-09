0cb1ffb7f9a6ddf6bea85bd415766cb918ead39e
AAKokate <kokateanirudhoff@gmail.com>
2023-10-09 09:23:31 +0530

    add project data

--- pages/Projects_for_you.py
+++ pages/Projects_for_you.py
@@ -5,9 +5,13 @@ import pandas as pd
 st.title("Projects for you")
 df = pd.DataFrame(
     [
-        {"command": "st.selectbox", "rating": 4, "is_widget": True},
-        {"command": "st.balloons", "rating": 5, "is_widget": False},
-        {"command": "st.time_input", "rating": 3, "is_widget": True},
+        {"Name": "port of mars", "Links": "https://github.com/topics/space-exploration"},
+        {"Name": "spacewalk", "Links": "https://github.com/spacewalkproject/spacewalk"},
+        {"Name": "PlanetProspect", "Links": "https://github.com/YassenEfremov/PlanetProspect"},
+        {"Name": "astrodata", "Links": "https://github.com/gvard/astrodata"},
+        {"Name": "Solar System", "Links": "https://github.com/JT5519/Solar-System"},
+        {"Name": "space debris", "Links": "https://github.com/galactic-embassy/space-debris"},
+        {"Name": "  space launches", "Links": "https://github.com/Quartz/space-launches"},
     ]
 )
 
